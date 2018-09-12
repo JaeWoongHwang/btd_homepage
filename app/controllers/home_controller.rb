6ff2a9b96d33b0d6e04bce3c94083ece0e4a1d64
JaeWoongHwang <jwngh15@gmail.com>
2018-09-12 18:16:03 +0900

    Updating some css effect

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
   end
-
+  
   def get_fullpage
     respond_to do |format|
       format.xml { render :json => Rails.application.credentials.fullpage_license.to_s }
